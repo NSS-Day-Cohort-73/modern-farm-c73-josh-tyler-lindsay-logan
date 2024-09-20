90ea3c1cc2455446323b6c0ef224e3f00e667e3b
Joshua Smith <mtgmirage@protonmail.com>
2024-09-20 14:35:30 -0500

    Update asparagus.js

--- src/scripts/seeds/asparagus.js
+++ src/scripts/seeds/asparagus.js
@@ -1,7 +1,12 @@
-export const createAsparagus = () => ({
+export const createAsparagus = () => {
+    const asparagusSeed = {
     type: "Asparagus",
     height: 24,
     output: 4
-});
+    };
+
+    return asparagusSeed;
+    
+};
 
 
