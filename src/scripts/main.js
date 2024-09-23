7eb2ca209ad4eb7fd57931056926ecccc4f10e14
Joshua Smith <mtgmirage@protonmail.com>
2024-09-23 10:03:42 -0500

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,3 @@
 import { createPlan } from "./plan.js";
 
 const yearlyPlan = createPlan();
-
-// import { createAsparagus } from "./seeds/asparagus.js"
-
-// const asparagusSeed = createAsparagus()
-// console.log(asparagusSeed)
